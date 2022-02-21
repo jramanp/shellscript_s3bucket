7a86ea2bbe38ca21624545ba5bb4ef946d675b57
jramanp <38029984+jramanp@users.noreply.github.com>
2022-02-21 15:33:15 +0530

    Update Backup.sh

--- Backup.sh
+++ Backup.sh
@@ -4,12 +4,13 @@ File=config1.yaml
 if [ -f "$File" ];
 then
 zip config.zip $File
+aws s3 cp ./config.zip s3://ct-123-deploy/success/
 echo "$File is exist"
 else
 echo "$File does not exist"  
 fi
 
 #upload Zip file to S3 bucket 
-echo "Coping the file S3 Bucket"
-aws s3 cp ./config.zip s3://ct-123-deploy/success/
-echo "Zip file has been uploaded to S3 Bucket"
+#echo "Copy the file to S3 Bucket"
+#aws s3 cp ./config.zip s3://ct-123-deploy/success/
+#echo "Zip file has been uploaded to S3 Bucket"
